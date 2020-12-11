f2b29288a27da6930c1ae35986ef08d718ff426b
amarendrakumar <amarendra.kumar@thoughtworks.com>
2020-12-11 23:25:14 +0530

    React Context

--- pages/speakers.tsx
+++ pages/speakers.tsx
@@ -3,14 +3,33 @@ import Menu from "../src/components/Menu/Menu";
 import SpeakerSearchBar from "../src/components/SpeakerSearchBar/SpeakerSearchBar";
 import Speakers from "../src/components/Speakers/Speakers";
 import Footer from "../src/components/Footer/Footer";
+import React from "react";
+import { SpeakerContext } from "../src/components/context/SpeakerContext";
+
+const speakers = [
+    {
+        imageSrc: 'speaker-component-1124.png',
+        name: 'Douglas CrockFord'
+    },
+    {
+        imageSrc: 'speaker-component-1530.png',
+        name: 'Tamara Baker'
+    },
+    {
+        imageSrc: 'speaker-component-10803.png',
+        name: 'Eugene Chuvyron'
+    }
+]
 
 const Page = () => {
     return (
         <div>
             <Header/>
             <Menu/>
-            <SpeakerSearchBar/>
-            <Speakers/>
+            <SpeakerContext.Provider value={speakers}>
+                <SpeakerSearchBar/>
+                <Speakers/>
+            </SpeakerContext.Provider>
             <Footer/>
         </div>
     )
--- src/components/Speakers/Speakers.tsx
+++ src/components/Speakers/Speakers.tsx
@@ -1,20 +1,14 @@
-import SpeakerRenderProps from "./SpeakerRenderProps";
+import {useContext} from "react";
+import {SpeakerContext} from "../context/SpeakerContext";
 
 const Speakers = () => {
+    const speakers = useContext(SpeakerContext);
     return (
-        <SpeakerRenderProps>
-            {({speakers}) => {
-                return (
-                    <div>
-                        {speakers.map(({imageSrc, name}) => {
-                                return <img src={`/images/${imageSrc}`} alt={name} key={imageSrc}></img>;
-                        })}
-                    </div>
-                )
-            }
-
-            }
-        </SpeakerRenderProps>
+        <div>
+            {speakers.map(({imageSrc, name}) => {
+                return <img src={`/images/${imageSrc}`} alt={name} key={imageSrc}></img>;
+            })}
+        </div>
     )
 }
 
